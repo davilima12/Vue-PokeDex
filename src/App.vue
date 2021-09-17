861ee7a93eba39c2e768091fa6c11b3c323f36f7
davilima12 <davidaer8847@gmail.com>
2021-09-17 19:24:41 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="is-offset-one-quarter ">
+      <img class="column is-half is-offset-one-quarter" src="./assets/pokemon-logo.jpg" id="pokemonimg" />
+      <hr />
+      <input
+        type="text"
+        class="input is-rounded column is-half is-offset-one-quarter"
+        placeholder="Buscar Pokemón Pelo Nome"
+        v-model="busca"
+        id="imput"
+      />
+      <hr/>
+
+      <div id="pokemondiv" >
+        <div v-for="(poke, index) in resultadoBusca" :key="poke.url" id="pokemon" class="column is-half is-one-third">
+          <pokemon :name="poke.name" :url="poke.url" :num="index + 1"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      busca: "",
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pegou a lista de pokemons");
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      });
+  },
   components: {
-    HelloWorld
+    Pokemon,
+  },
+  computed: {
+    resultadoBusca: function(){
+      if(this.busca == ""|| this.busca == " "){
+        return this.pokemons
+      }else{
+        return this.pokemons.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(this.busca.toLowerCase())
+        );
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgba(0, 0, 0, 0.555);
+ 
 }
+#pokemonimg {
+  border-radius: 3px;
+}
+#imput {
+  text-align: center;
+}
+
+#pokemon{
+  text-align: center;
+margin: 0px;
+}
+#pokemondiv{
+display: flex;
+flex-wrap: wrap;
+align-self: center;
+}
+
 </style>
